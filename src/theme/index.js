9ff077303d5ef44378bc6ff9d4a5bf57ac1adf81
ernestognw <ernestognw@gmail.com>
2019-11-02 15:05:49 -0500

    feature(theme): Updated theme within capital one colors.
    -
    
    No breaking changes

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,43 @@
 import BlockdemyUI from "blockdemy-ui";
+import Toast from "blockdemy-ui/toast";
 
-const { BlockdemyUIProvider } = new BlockdemyUI();
+const brand = {
+  primary: "#0D74AF",
+  secondary: "#F8CC01",
+  info: "#33B1DD",
+  default: "#213443",
+  success: "#0EA218",
+  warning: "#F8CC01",
+  danger: "#D03027"
+};
 
-export default BlockdemyUIProvider;
+const softColors = {};
+Object.keys(brand).forEach(color => (softColors[color] = `${brand[color]}33`));
+
+const grey = {
+  darker: "#121217",
+  dark: "#151A1F",
+  semiDark: "#474B4F",
+  lightDark: "#6F737A",
+  lightGrey: "#AAAEB3",
+  veryLightGrey: "#ECEDEF",
+  light: "#F2F2F2",
+  lighter: "#FFFFFF"
+};
+
+const colors = {
+  ...brand,
+  ...grey,
+  gradient: "linear-gradient(135deg, #30CEE7, #8265FC);"
+};
+
+const theme = {
+  colors,
+  softColors
+};
+
+const { toast } = new Toast(theme);
+
+const { BlockdemyUIProvider } = new BlockdemyUI(theme);
+
+export { BlockdemyUIProvider, toast };
