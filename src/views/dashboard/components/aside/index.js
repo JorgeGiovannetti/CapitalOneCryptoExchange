cfd056a1528bfa2526d4c047b59e132ea028b801
ernestognw <ernestognw@gmail.com>
2019-11-03 10:21:20 -0600

    Balances

--- src/views/dashboard/components/aside/index.js
+++ src/views/dashboard/components/aside/index.js
@@ -20,6 +20,7 @@ import { List, ListItem, ListItemText } from "blockdemy-ui/list";
 import { toast } from "theme";
 import Box from "blockdemy-ui/box";
 import Tooltip from "blockdemy-ui/tooltip";
+import abi from "./abi.json";
 
 class Aside extends Component {
   constructor(props) {
@@ -28,7 +29,11 @@ class Aside extends Component {
     this.state = {
       loading: false,
       installOpen: false,
-      connected: false
+      connected: false,
+      DAIBalance: 0,
+      BATBalance: 0,
+      USDTalance: 0,
+      REPBalance: 0
     };
   }
 
@@ -40,12 +45,58 @@ class Aside extends Component {
         this.setState({ loading: true });
         await window.ethereum.enable();
         const accounts = await this.web3.eth.getAccounts();
-        this.setState({
+        await this.setState({
           loading: false,
           connected: true,
           account: accounts[0]
         });
-        console.log(accounts[0]);
+
+        this.setState({
+          ETHBalance: await this.web3.eth.getBalance(accounts[0])
+        });
+
+        const { account } = this.state;
+
+        this.DAI = new this.web3.eth.Contract(
+          abi,
+          "0xf4578936f4d766b6626ffd519197cd02affcdde4"
+        );
+
+        this.BAT = new this.web3.eth.Contract(
+          abi,
+          "0x381360180f3da784a32bdf97662a880b5c6bb37a"
+        );
+
+        this.USDT = new this.web3.eth.Contract(
+          abi,
+          "0xd1e2784359613d3245324405c2d977e59f98235c"
+        );
+
+        this.REP = new this.web3.eth.Contract(
+          abi,
+          "0x987285956d76d2a07377ec8966963218837a6bbf"
+        );
+
+        this.DAI.methods
+          .balanceOf(account)
+          .call({ from: account }, (error, result) =>
+            this.setState({ DAIBalance: result })
+          );
+        this.BAT.methods
+          .balanceOf(account)
+          .call({ from: account }, (error, result) =>
+            this.setState({ BATBalance: result })
+          );
+        this.USDT.methods
+          .balanceOf(account)
+          .call({ from: account }, (error, result) =>
+            this.setState({ USDTBalance: result })
+          );
+        this.REP.methods
+          .balanceOf(account)
+          .call({ from: account }, (error, result) =>
+            this.setState({ REPBalance: result })
+          );
       } catch (err) {
         toast.info(
           "An error has occurred",
@@ -58,11 +109,19 @@ class Aside extends Component {
 
   copyIdToClipboard = () => {
     navigator.clipboard.writeText(this.state.account);
-  }
+  };
 
   render() {
-    console.log(this.props.user.accounts);
-    const { loading, connected, account } = this.state;
+    const {
+      loading,
+      connected,
+      account,
+      ETHBalance,
+      DAIBalance,
+      BATBalance,
+      USDTBalance,
+      REPBalance
+    } = this.state;
     return (
       <Container>
         <PriceContainer>
@@ -72,33 +131,37 @@ class Aside extends Component {
         {connected ? (
           <InputContainer>
             <EthAddress disabled value={account} />
-            <Tooltip
-              position="bottom"
-              tag="Copy to clipboard"
-            >
-              <CopyBtn onClick={this.copyIdToClipboard} size="small" color="lighterGrey" variant="soft" mt={3} fullWidth>
+            <Tooltip position="bottom" tag="Copy to clipboard">
+              <CopyBtn
+                onClick={this.copyIdToClipboard}
+                size="small"
+                color="lighterGrey"
+                variant="soft"
+                mt={3}
+                fullWidth
+              >
                 <CopyIcon></CopyIcon>
               </CopyBtn>
             </Tooltip>
           </InputContainer>
         ) : (
-            <Button
-              variant="soft"
-              mx="auto"
-              color="primary"
-              size="small"
-              mt={10}
-              onClick={this.connect}
-            >
-              {loading ? (
-                <Loader color="primary" size="15" />
-              ) : (
-                  <Fragment>
-                    <Icon src="/static/images/icons/metamask.svg" /> Connect
+          <Button
+            variant="soft"
+            mx="auto"
+            color="primary"
+            size="small"
+            mt={10}
+            onClick={this.connect}
+          >
+            {loading ? (
+              <Loader color="primary" size="15" />
+            ) : (
+              <Fragment>
+                <Icon src="/static/images/icons/metamask.svg" /> Connect
               </Fragment>
-                )}
-            </Button>
-          )}
+            )}
+          </Button>
+        )}
         <Typography mt={60} variant="muted">
           Currencies
         </Typography>
@@ -111,7 +174,7 @@ class Aside extends Component {
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/ethereum.png" />
-              <ListItemText primary="Ethereum" secondary="$150" />
+              <ListItemText primary="Ethereum" secondary={`$${ETHBalance}`} />
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/ethereum-classic.png" />
@@ -119,40 +182,40 @@ class Aside extends Component {
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/dai.png" />
-              <ListItemText primary="Dai" secondary="$192.0" />
+              <ListItemText primary="Dai" secondary={`$${DAIBalance}`} />
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/basic-attention-token.svg" />
               <ListItemText
                 primary="Basic Attention Token"
-                secondary="$122.2"
+                secondary={`$${BATBalance}`}
               />
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/tether.png" />
-              <ListItemText primary="Tether" secondary="$0" />
+              <ListItemText primary="Tether" secondary={`$${USDTBalance}`} />
             </ListItem>
             <ListItem clickable>
               <Avatar src="/static/images/currencies/augur.png" />
-              <ListItemText primary="Augur" secondary="$122.2" />
+              <ListItemText primary="Augur" secondary={`$${REPBalance}`} />
             </ListItem>
           </List>
         ) : (
-            <Fragment>
-              <List>
-                <ListItem clickable>
-                  <Avatar src="/static/images/currencies/capital-one.png" />
-                  <ListItemText primary="Capital One" secondary="$130" />
-                </ListItem>
-              </List>
-              <Box p={30}>
-                <Typography textAlign="center" variant="muted">
-                  You need to enable your Ethereum wallet to see your crypto
-                  balance
+          <Fragment>
+            <List>
+              <ListItem clickable>
+                <Avatar src="/static/images/currencies/capital-one.png" />
+                <ListItemText primary="Capital One" secondary="$130" />
+              </ListItem>
+            </List>
+            <Box p={30}>
+              <Typography textAlign="center" variant="muted">
+                You need to enable your Ethereum wallet to see your crypto
+                balance
               </Typography>
-              </Box>
-            </Fragment>
-          )}
+            </Box>
+          </Fragment>
+        )}
       </Container>
     );
   }
